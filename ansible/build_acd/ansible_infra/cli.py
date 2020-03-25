5c299e44247c87246d6e4ea48098f7dbbf3acc45
Toshio Kuratomi <a.badger@gmail.com>
2020-03-24 22:52:35 -0700

    Finished work on downloading and installing collections
    Still need to create a setup.py file and install it

--- ansible/build_acd/ansible_infra/cli.py
+++ ansible/build_acd/ansible_infra/cli.py
@@ -7,29 +7,46 @@
 
 import argparse
 import asyncio
+import os
 import os.path
+import pkgutil
 import sys
+import tempfile
 from urllib.parse import urljoin
 
 import aiohttp
 import semantic_version as semver
+import sh
+from mako.template import Template
 
-from .dependency_files import InvalidFileFormat, write_build_file, parse_build_file
-from .galaxy import GalaxyClient#, get_best_versions
+from .dependency_files import InvalidFileFormat, BuildFile, parse_pieces_file, write_deps_file
+from .galaxy import CollectionDownloader, GalaxyClient
 
 
-PIECES_FILE = 'acd.in'
-BUILD_FILE_TMPL = os.path.splitext(PIECES_FILE)[0] + '-{acd_version}.build'
-DEPS_FILE_TMPL = os.path.splitext(PIECES_FILE)[0] + '-{acd_complete_version}.lst'
+DEFAULT_FILE_BASE = 'acd'
+DEFAULT_PIECES_FILE = f'{DEFAULT_FILE_BASE}.in'
 
 PYPI_SERVER_URL = 'https://test.pypi.org/'
 GALAXY_SERVER_URL = 'https://galaxy.ansible.com/'
 
 
+class InvalidArgumentError(Exception):
+    pass
+
+
 def parse_args(program_name, args):
     common_parser = argparse.ArgumentParser(add_help=False)
-    common_parser.add_argument('acd_version',
-                               help='The version of ACD that this will be for')
+    common_parser.add_argument('acd_version', type=semver.Version,
+                               help='The X.Y.Z version of ACD that this will be for')
+    common_parser.add_argument('--dest-dir', default='.',
+                              help='Directory to write the output to')
+
+    build_parser = argparse.ArgumentParser(add_help=False)
+    build_parser.add_argument('--build-file', default=None,
+                              help='File containing the list of collections with version ranges')
+    build_parser.add_argument('--deps-file', default=None,
+                              help='File which will be written containing the list of collections'
+                              ' at versions which were included in this version of ACD')
 
     parser = argparse.ArgumentParser(prog=program_name,
                                      description='Script to manage building ACD')
@@ -40,31 +57,54 @@ def parse_args(program_name, args):
                                        description='Generate a new build description from the'
                                        ' latest available versions of ansible-base and the'
                                        ' included collections')
+    new_parser.add_argument('--pieces-file', default=DEFAULT_PIECES_FILE,
+                            help='File containing a list of collections to include')
+    new_parser.add_argument('--build-file', default=None,
+                            help='File which will be written which contains the list'
+                            ' of collections with version ranges')
 
-    build_single_parser = subparsers.add_parser('build-single', parents=[common_parser],
+    build_single_parser = subparsers.add_parser('build-single',
+                                                parents=[common_parser, build_parser],
                                                 description='Build a single-file ACD')
 
-    build_multiple_parser = subparsers.add_parser('build-multiple', parents=[common_parser],
+    build_multiple_parser = subparsers.add_parser('build-multiple',
+                                                  parents=[common_parser, build_parser],
                                                   description='Build a multi-file ACD')
 
     args = parser.parse_args(args)
 
     #
-    # Validation
+    # Validation and coercion
     #
     if args.command is None:
-        print('Please specify a subcommand to run')
-        sys.exit(2)
+        raise InvalidArgumentError('Please specify a subcommand to run')
+
+    if not os.path.isdir(args.dest_dir):
+        raise InvalidArgumentError(f'{args.dest_dir} must be an existing directory')
+
+    if args.command == 'new-acd':
+        if not os.path.isfile(args.pieces_file):
+            raise InvalidArgumentError(f'The pieces file, {args.pieces_file} must already exist.'
+                                       ' It should contains one namespace.collection per line')
+
+        if args.build_file is None:
+            basename = os.path.basename(os.path.splitext(args.pieces_file)[0])
+            args.build_file = f'{basename}-{args.acd_version.major}.{args.acd_version.minor}.build'
+
+    if args.command in ('build-single', 'build-multiple'):
+        if args.build_file is None:
+            args.build_file = (os.path.splitext(DEFAULT_FILE_BASE)[0]
+                               + f'-{args.acd_version.major}.{args.acd_version.minor}.build')
+
+        if not os.path.isfile(args.build_file):
+            raise InvalidArgumentError(f'The build file, {args.build_file} must already exist.'
+                                       ' It should contains one namespace.collection per line')
+
+        if args.deps_file is None:
+            basename = os.path.basename(os.path.splitext(args.build_file)[0])
+            args.deps_file =f'{basename}-{args.acd_version}.deps'
 
     return args
-
-
-def parse_pieces(pieces_file):
-    with open(pieces_file, 'r') as f:
-        # One collection per line, ignoring comments and empty lines
-        collections = [c.strip() for line in f.readlines()
-                       if (c := line.strip()) and not c.startswith('#')]
-    return collections
 
 
 async def get_ansible_base_version(aio_session, pypi_server_url=PYPI_SERVER_URL):
@@ -84,6 +124,7 @@ def display_exception(loop, context):
 async def get_version_info(collections):
     loop = asyncio.get_running_loop()
     loop.set_exception_handler(display_exception)
+
     requestors = {}
     async with aiohttp.ClientSession() as aio_session:
         requestors['_ansible_base'] = asyncio.create_task(get_ansible_base_version(aio_session))
@@ -91,12 +132,13 @@ async def get_version_info(collections):
 
         for collection in collections:
             requestors[collection] = asyncio.create_task(
-                galaxy_client.get_collection_version_info(collection))
+                galaxy_client.get_versions(collection))
 
         collection_versions = {}
-        for collection_name, request in requestors.items():
-            await request
-            collection_versions[collection_name] = request.result()
+        responses = await asyncio.gather(*requestors.values())
+
+        for idx, collection_name in enumerate(requestors):
+            collection_versions[collection_name] = responses[idx]
 
     return collection_versions
 
@@ -128,36 +170,117 @@ def find_latest_compatible(ansible_base_version, raw_dependency_versions):
 
 
 def new_acd(args):
-    collections = parse_pieces(PIECES_FILE)
+    collections = parse_pieces_file(args.pieces_file)
     dependencies = asyncio.run(get_version_info(collections))
 
-    ansible_base_version = dependencies.pop('_ansible_base')
+    ansible_base_version = dependencies.pop('_ansible_base')[0]
     dependencies = find_latest_compatible(ansible_base_version, dependencies)
 
-    build_file = BUILD_FILE_TMPL.format(acd_version=args.acd_version)
-    write_build_file(build_file, args.acd_version, ansible_base_version, dependencies)
+    build_filename = os.path.join(args.dest_dir, args.build_file)
+    build_file = BuildFile(build_filename)
+    build_file.write(args.acd_version, ansible_base_version, dependencies)
 
     return 0
 
 
-async def download_collections(deps):
-    requestors = []
+async def download_collections(deps, download_dir):
+    requestors = {}
     async with aiohttp.ClientSession() as aio_session:
-        for collection_name, version_spec in deps:
-            requestors.append(asyncio.create_task(download_collection(name, version_spec)))
+        for collection_name, version_spec in deps.items():
+            downloader = CollectionDownloader(GALAXY_SERVER_URL, aio_session, download_dir)
+            requestors[collection_name] = asyncio.create_task(
+                downloader.retrieve(collection_name, version_spec, download_dir))
 
-    await asyncio.gather(requestors)
+        included_versions = {}
+        responses = await asyncio.gather(*requestors.values())
+        for idx, collection_name in enumerate(requestors):
+            included_versions[collection_name] = responses[idx]
+
+    return included_versions
+
+
+async def install_collections(version, tmp_dir):
+    loop = asyncio.get_running_loop()
+    ansible_dir = os.path.join(tmp_dir, f'ansible-{version}')
+    os.mkdir(ansible_dir)
+
+    installers = []
+    collection_tarballs = (p for f in os.listdir(tmp_dir)
+                           if os.path.isfile(p := os.path.join(tmp_dir, f)))
+    for filename in collection_tarballs:
+        installers.append(loop.run_in_executor(None, sh.ansible_galaxy, 'collection',
+                                               'install', '-p', ansible_dir,
+                                               filename, '-vvv'))
+    await asyncio.gather(*installers)
+
+
+def copy_boilerplate_files(package_dir):
+    gpl_license = pkgutil.get_data('ansible_infra', 'gplv3.txt')
+    with open(os.path.join(package_dir, 'COPYING'), 'wb') as f:
+        f.write(gpl_license)
+
+    readme = pkgutil.get_data('ansible_infra', 'acd-readme.txt')
+    with open(os.path.join(package_dir, 'README'), 'wb') as f:
+        f.write(readme)
+
+
+def write_manifest(package_dir, data_files):
+    manifest_file = os.path.join(package_dir, 'MANIFEST.IN')
+    with open(manifest_file, 'w') as f:
+        f.write('COPYING\n')
+        f.write('README\n')
+        f.write('\n'.join(data_files))
+
+
+def write_setup(package_dir, acd_version, python_dirs):
+    setup_tmpl = Template(pkgutil.get_data('ansible_infra', 'setup_py.mk'))
+    python_packages = "[ '%s' ]"
+
+    setup_contents = setup_tmpl.render(version=acd_version, python_packages=python_packages)
+    with open(setup_file, 'w') as f:
+        f.write(setup_contents)
+
+
+def write_python_build_files(acd_version, dest_dir):
+    toplevel_dir = os.path.join(dest_dir, f'ansible-{acd_version}')
+    package_dir = os.path.join(toplevel_dir, 'ansible_collections')
+
+    python_files = []
+    python_dirs = ['ansible_collections']
+    data_files = []
+    for root, directories, filenames in os.walk(package_dir):
+        root = root[len(package_dir) + 1:]
+        for filename in (os.path.join(root, f) for f in filenames):
+            if filename.endswith('.py'):
+                python_files.append(filename)
+            else:
+                data_files.append(filename)
+
+        for dir_name in (os.path.join(root, d) for d in directories):
+            python_dirs.append(dir_name)
+
+
+    copy_boilerplate_files(toplevel_dir)
+    write_manifest(toplevel_dir, data_files)
+    write_setup(toplevel_dir, acd_version, python_dirs)
 
 
 def build_single(args):
-    build_file = BUILD_FILE_TMPL.format(acd_version=args.acd_version)
-    acd_version, ansible_base_version, deps = parse_build_file(build_file)
+    build_file = BuildFile(args.build_file)
+    build_acd_version, ansible_base_version, deps = build_file.parse()
+
+    if not str(args.acd_version).startswith(build_acd_version):
+        print(f'{args.build_file} is for version {build_acd_version} but we need'
+              ' {args.acd_version.major}.{arg.acd_version.minor}')
 
     with tempfile.TemporaryDirectory() as download_dir:
-        asyncio.run(download_collections(concrete_deps, download_dir))
-        install_collections(download_dir, install_path)
-        write_out_setup()
-        make_dist()
+        included_versions = asyncio.run(download_collections(deps, download_dir))
+        asyncio.run(install_collections(args.acd_version, download_dir))
+        write_python_build_files(args.acd_version, download_dir)
+        #make_dist()
+
+    deps_filename = os.path.join(args.dest_dir, args.deps_file)
+    write_deps_file(deps_filename, args.acd_version, ansible_base_version, included_versions)
 
     return 0
 
@@ -175,6 +298,10 @@ ARGS_MAP = {'new-acd': new_acd,
 
 def main(args):
     program_name = os.path.basename(args[0])
-    args = parse_args(program_name, args[1:])
+    try:
+        args = parse_args(program_name, args[1:])
+    except InvalidArgumentError as e:
+        print(e)
+        return 2
 
     return ARGS_MAP[args.command](args)
--- ansible/build_acd/ansible_infra/dependency_files.py
+++ ansible/build_acd/ansible_infra/dependency_files.py
@@ -16,49 +16,76 @@ class InvalidFileFormat(Exception):
     pass
 
 
-def parse_build_file(build_file):
-    """
-    Parse the build from a dependency file
-    """
-    deps = {}
-    ansible_base_version = acd_version = None
-    with open(build_file, 'r') as f:
-        for line in f:
-            record = line.strip().split(':', 1)
-
-            if record[0] == '_acd_version':
-                if acd_version is not None:
-                    raise InvalidFileFormat(f'{build_file} specified _acd_version more than once')
-                acd_version = record[1]
-                continue
-
-            if record[0] == '_ansible_base_version':
-                if ansible_base_version is not None:
-                    raise InvalidFileFormat(f'{build_file} specified _ansible_base_version more'
-                                            ' than once')
-                ansible_base_version = record[1]
-                continue
-
-            versions = record[1].split(',')
-            deps[record[0]] = versions
-
-    if ansible_base_version is None or acd_version is None:
-        raise InvalidFileFormat(f'{build_file} was invalid.  It did not contain required fields')
-
-    return acd_version, ansible_base_version, deps
+def parse_pieces_file(pieces_file):
+    with open(pieces_file, 'r') as f:
+        # One collection per line, ignoring comments and empty lines
+        collections = [c.strip() for line in f.readlines()
+                       if (c := line.strip()) and not c.startswith('#')]
+    return collections
 
 
-def write_build_file(build_file, acd_version, ansible_base_version, dependencies):
-    """
-    Write a build dependency file
+def write_deps_file(deps_file, acd_version, ansible_base_version, included_versions):
+    """Write a list of all the dependent collections included in this ACD release"""
+    records = []
+    for dep, version in included_versions.items():
+        records.append(f'{dep}: {version}')
+    records.sort()
 
-    """
-    with open(build_file, 'w') as f:
-        f.write(f'_acd_version:{acd_version}\n')
-        f.write(f'_ansible_base_version:{ansible_base_version[0]}\n')
+    with open(deps_file, 'w') as f:
+        f.write(f'_acd_version: {acd_version}\n')
+        f.write(f'_ansible_base_version: {ansible_base_version[0]}\n')
+        f.write('\n'.join(records))
+        f.write('\n')
+
+
+class BuildFile:
+    def __init__(self, build_file):
+        self.filename = build_file
+
+    def parse(self):
+        """
+        Parse the build from a dependency file
+        """
+        deps = {}
+        ansible_base_version = acd_version = None
+        with open(self.filename, 'r') as f:
+            for line in f:
+                record = [entry.strip() for entry in line.split(':', 1)]
+
+                if record[0] == '_acd_version':
+                    if acd_version is not None:
+                        raise InvalidFileFormat(f'{self.filename} specified _acd_version'
+                                                ' more than once')
+                    acd_version = record[1]
+                    continue
+
+                if record[0] == '_ansible_base_version':
+                    if ansible_base_version is not None:
+                        raise InvalidFileFormat(f'{self.filename} specified _ansible_base_version'
+                                                ' more' ' than once')
+                    ansible_base_version = record[1]
+                    continue
+
+                deps[record[0]] = record[1]
+
+        if ansible_base_version is None or acd_version is None:
+            raise InvalidFileFormat(f'{self.filename} was invalid.  It did not contain'
+                                    ' required fields')
+
+        return acd_version, ansible_base_version, deps
+
+    def write(self, acd_version, ansible_base_version, dependencies):
+        """
+        Write a build dependency file
+
+        """
+        records = []
         for dep, version in dependencies.items():
-            major_ver = version.major
-            next_major_ver = major_ver + 1
-            f.write(f'{dep}:>={major_ver}.0.0,<{next_major_ver}.0.0\n')
+            records.append(f'{dep}: >={version.major}.0.0,<{version.next_major()}')
+        records.sort()
 
-
+        with open(self.filename, 'w') as f:
+            f.write(f'_acd_version: {acd_version.major}.{acd_version.minor}\n')
+            f.write(f'_ansible_base_version: {ansible_base_version}\n')
+            f.write('\n'.join(records))
+            f.write('\n')
